fdc5968e916b0f0c6a818e9d4fb4bfd024964579
Abinash Mohapatra <abinash@expensebit.com>
2020-10-20 12:49:49 +0530

    First commit

---
+++ public/precache-manifest.d91689d971aaee993a61b1b3cbe42df8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8170d5562d1ea78e2bef8bcedc1cd631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb81454edaf8650a148",
+    "url": "/static/css/2.c6c0dec4.chunk.css"
+  },
+  {
+    "revision": "bd7f779b67e299cc3994",
+    "url": "/static/css/main.eb77765e.chunk.css"
+  },
+  {
+    "revision": "9fb81454edaf8650a148",
+    "url": "/static/js/2.e6d03d1c.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.e6d03d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7f779b67e299cc3994",
+    "url": "/static/js/main.22642886.chunk.js"
+  },
+  {
+    "revision": "14a926e5a3ab9b0224ac",
+    "url": "/static/js/runtime-main.62527498.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
